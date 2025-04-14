e8584ddf394ec9620e402ab940ff47fa986c59ab
Bielandrade2007 <ga490899@gmail.com>
2025-04-14 20:21:53 -0300

    Classe produto modelo criada

--- backend/produtos/src/main/java/br/com/crud/produtos/ProdutosApplication.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/ProdutosApplication.java
@@ -1,4 +1,4 @@
-package br.com.crud.produtos.produtos;
+package br.com.crud.produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
