7881322708bfa65560be48ca8a10eee3226a8a91
Bielandrade2007 <ga490899@gmail.com>
2025-04-14 20:32:23 -0300

    Criação da classe Resposta modelo

---
+++ backend/produtos/src/main/java/br/com/crud/produtos/modelo/RespostaModelo.java
@@ -0,0 +1,13 @@
+package br.com.crud.produtos.modelo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Setter
+@Getter
+public class RespostaModelo {
+  private String resposta;
+}
--- backend/produtos/src/main/java/br/com/crud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/servico/ProdutoServico.java
@@ -14,4 +14,5 @@ public class ProdutoServico {
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
     }
+    
 }
