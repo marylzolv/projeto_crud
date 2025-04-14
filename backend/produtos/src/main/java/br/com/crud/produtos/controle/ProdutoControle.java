261490b6703519f83525810db95e1534bf8e8259
Bielandrade2007 <ga490899@gmail.com>
2025-04-14 20:34:17 -0300

    Criação da rota remover

--- backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
@@ -2,17 +2,27 @@ package br.com.crud.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.crud.produtos.modelo.ProdutoModelo;
+import br.com.crud.produtos.modelo.RespostaModelo;
 import br.com.crud.produtos.servico.ProdutoServico;
 
+@RestController
 public class ProdutoControle {
   @Autowired
   private ProdutoServico ps;
+
+  @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
+    }
 
   @PostMapping("/cadastrar")
   public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
--- backend/produtos/src/main/java/br/com/crud/produtos/servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/servico/ProdutoServico.java
@@ -11,7 +11,7 @@ import br.com.crud.produtos.repositorio.ProdutoRepositorio;
 
 @Service
 public class ProdutoServico {
-  
+
   @Autowired
   private ProdutoRepositorio pr;
 
@@ -38,4 +38,11 @@ public class ProdutoServico {
             }
         }
     }
+
+    public ResponseEntity<RespostaModelo> remover(long codigo) {
+      pr.deleteById(codigo);
+      rm.setResposta("O produto foi removido com sucesso!");
+      return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+    }
+    
 }
