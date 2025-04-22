5642973041ca48ba014f78b01a2efc131086a01f
jabesz <jabesvieira4@gmail.com>
2025-04-22 08:30:42 -0300

    Estilização e visibilidade de botões

--- frontend/produtos/src/App.js
+++ frontend/produtos/src/App.js
@@ -1,15 +1,18 @@
+
+import { useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
+  const [btnCadastrar] = useState(false);
+
   return (
-    <div className="App">
-      <Formulario />
+    <div>
+      <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
-
   )
 }
 
--- frontend/produtos/src/Formulario.jsx
+++ frontend/produtos/src/Formulario.jsx
@@ -1,14 +1,18 @@
-function Formulario() {
+function Formulario({ botao }) {
   return (
     <form>
       <h1>Formulario</h1>
-      <input type="text" placeholder="Nome"></input>
-      <input type="text" placeholder="Marca"></input>
-
-      <input type="button" value="Cadastrar" />
-      <input type="button" value="Cancelar" />
-      <input type="button" value="Alterar" />
-      <input type="button" value="Remover" />
+      <input className="form-control" type="text" placeholder="Nome"></input>
+      <input className="form-control" type="text" placeholder="Marca"></input>
+      {botao ? (
+        <input className="btn btn-success" type="button" value="Cadastrar" />
+      ) : (
+        <div>
+          <input className="btn btn-warning" type="button" value="Cancelar" />
+          <input className="btn btn-primary" type="button" value="Alterar" />
+          <input className="btn btn-danger" type="button" value="Remover" />
+        </div>
+      )}
     </form>
   );
 }
