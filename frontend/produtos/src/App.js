712c7961a07d2e737f0c03dcaece44379d74c71f
jabez <jabesvieira4@gmail.com>
2025-04-29 09:55:03 -0300

    front conectado ao back

--- frontend/produtos/src/App.js
+++ frontend/produtos/src/App.js
@@ -1,17 +1,27 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
-  const [btnCadastrar] = useState(false);
+  const [btnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
 
   return (
     <div>
       <Formulario botao={btnCadastrar} />
-      <Tabela />
+      <Tabela vetor={produtos} />
     </div>
   )
 }
